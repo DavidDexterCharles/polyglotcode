564889a8f8c651a40934e6bc900126b0e6ed0024
ICE-WOLF <david.dexter.charles@gmail.com>
2016-01-01 13:26:22 -0400

    php api implemented update

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php 
+
+	//https://github.com/KnpLabs/php-github-api/blob/master/doc/repos.md
 	
 	require_once 'vendor/autoload.php';
 	$client = new \Github\Client();
 
 	$repos = $client->api('user')->repositories('ICE-WOLF');
 
-	var_dump($repos);
+	//print_r ($repos);
 
 
+	$following = $client->api('user')->following('ICE-WOLF');
+	//print_r($following[3]);
 
+	$followers = $client->api('user')->followers('ICE-WOLF');
+	print_r($followers[3]);
 
  ?>
